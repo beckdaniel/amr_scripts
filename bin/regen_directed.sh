7c6408cd888b2b3a99446d3d60178958385c870f
Daniel Beck <daniel.e.beck@gmail.com>
2017-11-15 15:27:47 +1100

    added pasted sets for decoding

--- bin/regen_directed.sh
+++ bin/regen_directed.sh
@@ -1,6 +1,11 @@
 python preproc_amr.py ../data/preproc/train/amr.en ../data/preproc/train/surface.en ../data/directed/train.nodes.en ../data/directed/train.surface.en --mode GRAPH --triples ../data/directed/train.triples.en
 python preproc_amr.py ../data/preproc/dev/amr.en ../data/preproc/dev/surface.en ../data/directed/dev.nodes.en ../data/directed/dev.surface.en --mode GRAPH --triples ../data/directed/dev.triples.en
 python preproc_amr.py ../data/preproc/test/amr.en ../data/preproc/test/surface.en ../data/directed/test.nodes.en ../data/directed/test.surface.en --mode GRAPH --triples ../data/directed/test.triples.en
+
 python preproc_vocab.py ../data/directed/train.triples.en ../data/directed/train.triples.unk.freq$1.en ../data/directed/edge_vocab_freq$1.json --filter FREQ --value $1
 python preproc_vocab.py ../data/directed/test.triples.en ../data/directed/test.triples.unk.freq$1.en ../data/directed/edge_vocab_freq$1.json --mode UNK
 python preproc_vocab.py ../data/directed/dev.triples.en ../data/directed/dev.triples.unk.freq$1.en ../data/directed/edge_vocab_freq$1.json --mode UNK
+
+paste ../data/directed/train.nodes.en ../data/directed/train.triples.unk.freq$1.en > ../data/directed/train.nodes_triples.unk.freq$1.en
+paste ../data/directed/dev.nodes.en ../data/directed/dev.triples.unk.freq$1.en > ../data/directed/dev.nodes_triples.unk.freq$1.en
+paste ../data/directed/test.nodes.en ../data/directed/test.triples.unk.freq$1.en > ../data/directed/test.nodes_triples.unk.freq$1.en
--- bin/regen_undirected.sh
+++ bin/regen_undirected.sh
@@ -1,7 +1,11 @@
 python preproc_amr.py ../data/preproc/train/amr.en ../data/preproc/train/surface.en ../data/undirected/train.nodes.en ../data/undirected/train.surface.en --mode GRAPH --triples ../data/undirected/train.triples.en --add-reverse
 python preproc_amr.py ../data/preproc/dev/amr.en ../data/preproc/dev/surface.en ../data/undirected/dev.nodes.en ../data/undirected/dev.surface.en --mode GRAPH --triples ../data/undirected/dev.triples.en --add-reverse
 python preproc_amr.py ../data/preproc/test/amr.en ../data/preproc/test/surface.en ../data/undirected/test.nodes.en ../data/undirected/test.surface.en --mode GRAPH --triples ../data/undirected/test.triples.en --add-reverse
+
 python preproc_vocab.py ../data/undirected/train.triples.en ../data/undirected/train.triples.unk.freq$1.en ../data/undirected/edge_vocab_freq$1.json --filter FREQ --value $1
 python preproc_vocab.py ../data/undirected/test.triples.en ../data/undirected/test.triples.unk.freq$1.en ../data/undirected/edge_vocab_freq$1.json --mode UNK
 python preproc_vocab.py ../data/undirected/dev.triples.en ../data/undirected/dev.triples.unk.freq$1.en ../data/undirected/edge_vocab_freq$1.json --mode UNK
 
+paste ../data/undirected/train.nodes.en ../data/undirected/train.triples.unk.freq$1.en > ../data/undirected/train.nodes_triples.unk.freq$1.en
+paste ../data/undirected/dev.nodes.en ../data/undirected/dev.triples.unk.freq$1.en > ../data/undirected/dev.nodes_triples.unk.freq$1.en
+paste ../data/undirected/test.nodes.en ../data/undirected/test.triples.unk.freq$1.en > ../data/undirected/test.nodes_triples.unk.freq$1.en
